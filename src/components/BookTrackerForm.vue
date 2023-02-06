505581bb183ab400be5b4256bd6d987c7f40216f
ilatine <ilatine@gmail.com>
2023-02-06 18:42:20 -0500

    Initial book tracker application

---
+++ src/components/BookTrackerForm.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import Card from "primevue/card";
+import InputText from "primevue/inputtext";
+import { ref } from "vue";
+
+const name = ref("");
+const author = ref("");
+const totalPages = ref(0);
+const started = ref("");d
+</script>
+
+<template>
+  <Card>
+    <template #title> Book Tracker Form </template>
+    <template #content>
+      <div>
+        <label>Book Name: </label
+        ><InputText id="name" type="text" v-model="name" />
+      </div>
+
+      <div>
+        <label>Author: </label
+        ><InputText id="author" type="text" v-model="author" />
+      </div>
+
+      <div>
+        <label>Total Pages: </label
+        ><InputText id="totalPages" type="text" v-model="totalPages" />
+      </div>
+
+      <div>
+        <label>Have you started?: </label
+        ><InputText id="started" type="text" v-model="started" />
+      </div>
+    </template>
+    <template #footer>
+      <Button icon="pi pi-check" label="Save">Save</Button>
+      <Button
+        icon="pi pi-times"
+        label="Cancel"
+        class="p-button-secondary"
+        style="margin-left: 0.5em"
+        >Cancel</Button
+      >
+    </template>
+  </Card>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+import PrimeVue from "primevue/config";
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue);
+
+app.mount("#app");
